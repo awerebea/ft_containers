6270dc4ab2e9f2b276e95401e5f7878b568797ac
awerebea (21-school) <awerebea.21@gmail.com>
2021-01-26 17:58:50 +0300

    LIST Add (con|de)structor, methods, nested class
    Constructor: copy.
    Destructor: default.
    Methods: push_front, clear, operator=.
    Class: const_iterator.

--- src/List.h
+++ src/List.h
@@ -29,9 +29,9 @@ namespace ft {
 
 	private:
 		typedef struct	s_node {
-			T *			data;
-			s_node *	prev;
-			s_node *	next;
+			value_type *	data;
+			s_node *		prev;
+			s_node *		next;
 		}				t_node;
 
 		typedef typename allocator_type::template rebind<t_node>::other
@@ -44,6 +44,8 @@ namespace ft {
 
 		void			end_node_init() {
 			end_node = alloc_rebind.allocate(1);
+			end_node->data = alloc.allocate(1);
+			alloc.construct(end_node->data, value_type());
 			end_node->next = end_node;
 			end_node->prev = end_node;
 			size = 0;
@@ -59,74 +61,153 @@ namespace ft {
 
 	public:
 
+		/*
+		* Iterators
+		*/
+
+		class const_iterator;
+
 		class iterator : public std::bidirectional_iterator_tag {
 			t_node *	node;
 		public:
 			iterator(): node(NULL) {}
 
-			iterator(t_node * node): node(node) {}
+			explicit iterator(t_node * node): node(node) {}
 
-			iterator(iterator const & other) { *this = other; }
+			iterator(const iterator& other) { *this = other; }
 
 			~iterator() {}
 
-			iterator &	operator=(iterator const & other) {
-				if (this != *other) { node = other.node; }
+			iterator&			operator=(const iterator& other) {
+				if (this != &other) { node = other.node; }
 				return *this;
 			}
 
-			bool		operator==(iterator const & other) {
+			bool				operator==(const iterator& other) const {
 				return node == other.node;
 			};
 
-			bool		operator!=(iterator const & other) {
+			bool				operator==(const const_iterator& other) const {
+				return node == other.getNode();
+			};
+
+			bool				operator!=(const iterator& other) const {
 				return node != other.node;
 			};
 
-			T &			operator*() {
+			bool				operator!=(const const_iterator& other) const {
+				return node != other.getNode();
+			};
+
+			value_type&		operator*() const {
 				return *node->data;
 			};
 
-			T *			operator&() {
+			value_type *		operator->() const {
 				return node->data;
 			};
 
-			iterator &	operator++() {
+			iterator&			operator++() {
 				node = node->next;
 				return *this;
 			};
 
-			iterator	operator++(int) {
+			iterator			operator++(int) {
 				iterator	ret = *this;
 				node = node->next;
 				return ret;
 			};
 
-			iterator &	operator--() {
+			iterator&			operator--() {
 				node = node->prev;
 				return *this;
 			};
 
-			iterator	operator--(int) {
+			iterator			operator--(int) {
 				iterator	ret = *this;
 				node = node->prev;
 				return ret;
 			};
 
-			t_node *	getNode() const { return node; }
-		};
+			t_node *			getNode() const { return node; }
+		}; // class iterator
+
+		class const_iterator : public std::bidirectional_iterator_tag {
+			t_node *	node;
+		public:
+			const_iterator(): node(NULL) {}
+
+			explicit const_iterator(t_node * node): node(node) {}
+
+			const_iterator(const const_iterator& other) { *this = other; }
+
+			~const_iterator() {}
+
+			const_iterator&	operator=(const const_iterator& other) {
+				if (this != &other) { node = other.node; }
+				return *this;
+			}
+
+			bool				operator==(const const_iterator& other) const {
+				return node == other.node;
+			};
+
+			bool				operator==(const iterator& other) const {
+				return node == other.getNode();
+			};
+
+			bool				operator!=(const const_iterator& other) const {
+				return node != other.node;
+			};
+
+			bool				operator!=(const iterator& other) const {
+				return node != other.getNode();
+			};
+
+			const value_type&	operator*() const {
+				return *node->data;
+			};
+
+			const value_type *	operator->() const {
+				return node->data;
+			};
+
+			const_iterator&	operator++() {
+				node = node->next;
+				return *this;
+			};
+
+			const_iterator		operator++(int) {
+				const_iterator	ret = *this;
+				node = node->next;
+				return ret;
+			};
+
+			const_iterator&	operator--() {
+				node = node->prev;
+				return *this;
+			};
+
+			const_iterator		operator--(int) {
+				const_iterator	ret = *this;
+				node = node->prev;
+				return ret;
+			};
+
+			t_node *			getNode() const { return node; }
+		}; // class const_iterator
 
 		/*
 		* Constructors:
 		*/
 
 		/* default (1) */
-		explicit List (const allocator_type& alloc = allocator_type()) {
+		explicit	List(const allocator_type& alloc = allocator_type()) {
 			end_node_init();
 		}
 
 		/* fill (2) */
-		explicit List (size_type n, const value_type& val = value_type(),
+		explicit	List(size_type n, const value_type& val = value_type(),
 						const allocator_type& alloc = allocator_type()) {
 			end_node_init();
 			for (size_type i = 0; i < n; ++i) {
@@ -140,15 +221,68 @@ namespace ft {
 		//                     const allocator_type& alloc = allocator_type());
 
 		/* copy (4) */
-		// List (const List& x);
+					List(const List& other) { *this = other; }
 
-		void push_back (const value_type& val) {
+					~List() {
+						clear();
+						alloc.destroy(end_node->data);
+						alloc.deallocate(end_node->data, 1);
+						alloc_rebind.deallocate(end_node, 1);
+					}
+
+		List&				operator=(const List& other) {
+			if (this == &other) { return *this; }
+			clear();
+			t_node * curr_this = end_node->next;
+			t_node * curr_other = other.end_node->next;
+			for (size_type i = 0; i < other.size; ++i) {
+				push_back(*curr_other->data);
+				curr_this = curr_this->next;
+				curr_other = curr_other->next;
+			}
+			return *this;
+		}
+
+		/*
+		* Iterators
+		*/
+
+		iterator	begin() { return iterator(end_node->next); }
+
+		iterator	end() { return iterator(end_node); }
+
+		/*
+		* Modifiers
+		*/
+
+		void		push_back(const value_type &val) {
 			t_node * tmp = end_node->prev;
 			t_node * new_node = new_node_init(val);
 			new_node->prev = end_node->prev;
 			new_node->next = end_node;
 			tmp->next = new_node;
 			end_node->prev = new_node;
+		}
+
+		void		push_front(const value_type &val) {
+			t_node * tmp = end_node->next;
+			t_node * new_node = new_node_init(val);
+			new_node->prev = end_node;
+			new_node->next = end_node->next;
+			tmp->prev = new_node;
+			end_node->next = new_node;
+		}
+
+		void		clear() {
+			t_node * tmp;
+			for (; size > 0; --size) {
+				tmp = end_node->next->next;
+				alloc.destroy(end_node->next->data);
+				alloc.deallocate(end_node->next->data, 1);
+				alloc_rebind.deallocate(end_node->next, 1);
+				end_node->next = tmp;
+			}
+			end_node->prev = end_node;
 		}
 
 	}; // class List
--- test/gtests_main.cpp
+++ test/gtests_main.cpp
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   gtests_main.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -11,6 +11,61 @@
 /* ************************************************************************** */
 
 #include <gtest/gtest.h>
+#include "List.h"
+#include <list>
 
-TEST(BasicTest, NumberCompare) {
+TEST(List, Constructors) {
+	// default
+	{
+		std::list<int>	lst_std;
+		ft::List<int>	lst_ft;
+		EXPECT_EQ(*lst_std.begin(), *lst_ft.begin());
+	}
+	// fill
+	{
+		std::list<int>	lst_std(2);
+		ft::List<int>	lst_ft(2);
+		EXPECT_EQ(*lst_std.begin(), *lst_ft.begin());
+	}
+	{
+		std::list<int>	lst_std(2, 5);
+		ft::List<int>	lst_ft(2, 5);
+		EXPECT_EQ(*lst_std.begin(), *lst_ft.begin());
+	}
+
+	{
+		std::list<int>	* lst_std = new std::list<int>(5, 5);
+		ft::List<int>	* lst_ft = new ft::List<int>(5, 5);
+
+		delete(lst_std);
+		delete(lst_ft);
+	}
+}
+
+TEST(List, Clear) {
+	std::list<int>	lst_std(2, 5);
+	ft::List<int>	lst_ft(2, 5);
+	std::cout << *lst_ft.begin() << std::endl;
+	EXPECT_EQ(*lst_std.begin(), *lst_ft.begin());
+	lst_std.clear();
+	lst_ft.clear();
+	std::cout << *lst_ft.begin() << std::endl;
+	EXPECT_EQ(*lst_std.begin(), *lst_ft.begin());
+}
+
+TEST(List, Iterators) {
+	std::list<int>	lst_std(2, 1);
+	ft::List<int>	lst_ft(2, 1);
+	// Begin
+	EXPECT_EQ(*lst_std.begin(), *lst_ft.begin());
+}
+
+TEST(List, Operator_Assign) {
+	ft::List<int>	lst_1(2, 1);
+	ft::List<int>	lst_2(5, 2);
+	std::cout << *lst_1.begin() << std::endl;
+	std::cout << *lst_2.begin() << std::endl;
+	lst_2 = lst_1;
+	std::cout << *lst_2.begin() << std::endl;
+	EXPECT_EQ(*lst_1.begin(), *lst_2.begin());
 }
